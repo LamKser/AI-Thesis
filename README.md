a99d613c2b7333acd4ebcaf7bd9697c74a7b8ee6
LamKser <dinhhoanglam2801118@gmail.com>
2023-09-17 00:58:14 +0700

    add border crop

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 * Advisor: Nguyen Quoc Trung
 * Dinh Hoang Lam
 * Tran Duy Ngoc Bao
+
+# How to run
+* `data`
+    
+    **Create LR HR pair data from HR images**
+    ```
+    python data/create_dataset.py --dir <image folder>
+    ```
+    
+    * Optional arguments
+        * `--border-scale` : crop border of image base on scale of image shape (default: 0)
+        * `--size` :  size of HR image (default: 96)
+        * `--step` : step of window crop (default: 96)
+        * `--scale` : scale factor for resizing image (default: 2)
+        * `--hr-dir` , `--lr-dir` : HR and LR folder for saving crop images (default: 'hr', 'lr')
+
--- data/create_dataset.py
+++ data/create_dataset.py
@@ -20,11 +20,12 @@ def get_pair_images(lr_directory, hr_directory, scale=4, step=96):
     pass
 
 
-def ger_hr_lr_patches(directory, scale=4, step=96, size=96,
+def ger_hr_lr_patches(directory, 
+                      border_scale=0.08, scale=4, step=96, size=96,
                       hr_save='hr', lr_save='lr'):
     files = os.listdir(directory)
-    os.makedirs(hr_save, exist_ok=True)
-    os.makedirs(lr_save, exist_ok=True)
+    # os.makedirs(hr_save, exist_ok=True)
+    # os.makedirs(lr_save, exist_ok=True)
 
     for f in files:
         name, ext = splitext(f)
@@ -35,30 +36,40 @@ def ger_hr_lr_patches(directory, scale=4, step=96, size=96,
             continue
 
         h, w = img.shape[:2]
-        for i, x in enumerate(range(0, h - size, step)):
-            for j, y in enumerate(range(0, w - size, step)):
+        xmin, ymin = int(w * border_scale), int(h * border_scale)
+        xmax, ymax = w -1 - int(w * border_scale), h - 1 - int(h * border_scale)
+
+        # cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 255), thickness=2)
+        # import matplotlib.pyplot as plt
+        # plt.imshow(img)
+        # plt.show()
+        for i, x in enumerate(range(xmin, xmax - size, step)):
+            for j, y in enumerate(range(ymin, ymax - size, step)):
                 hr_img = img[y : y + size, x : x + size]
                 lr_img = cv2.resize(hr_img, (size//scale, size//scale))
                 cv2.imwrite(join(hr_save, f'{name}_{i}{j}_{ext}'), hr_img)
                 cv2.imwrite(join(lr_save, f'{name}_{i}{j}_{ext}'), lr_img)
+        
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('--directory', '--dir', type=str,
+    parser.add_argument('--dir', type=str,
                         help='Images folder', required=True)
+    parser.add_argument('--border-scale', '--bs', type=float, default=0,
+                        help='Border scale')
     parser.add_argument('--size', '--si', type=int, default=96,
                         help='Size of HR image')
     parser.add_argument('--step', '--st', type=int, default=96,
                         help='Step crop image')
-    parser.add_argument('--scale', '--sa', type=int, default=4,
+    parser.add_argument('--scale', '--sa', type=int, default=2,
                         help='Scale factor')
-    parser.add_argument('--hr_directory', '--hr', type=str, default='hr',
+    parser.add_argument('--hr_dir', '--hr', type=str, default='hr',
                         help='hr_dir')
-    parser.add_argument('-lr_directory', '--lr', type=str, default='lr',
+    parser.add_argument('--lr_dir', '--lr', type=str, default='lr',
                         help='lr_dir')
     args = parser.parse_args()
 
-    ger_hr_lr_patches(args.directory, args.scale, args.step, args.size, args.hr_directory, args.lr_directory)
+    ger_hr_lr_patches(args.directory, args.border_scale, args.scale, args.step, args.size, args.hr_directory, args.lr_directory)
